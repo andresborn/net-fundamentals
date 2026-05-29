a864f701d17161ba644c3a0dca71917752f5ab64
Andres Born <andres.born@gmail.com>
2026-05-29 13:10:45 -0600

    Raw HTTP server

---
+++ raw-http-server/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
+
+	if err != nil {
+		log.Fatal("Error listening: ", err)
+	}
+
+	defer listener.Close()
+
+	log.Println("Listening...")
+
+	for {
+
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Println("Error accepting connection: ", err)
+		}
+
+		go handleConnection(conn)
+
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	_, err := conn.Read(buffer)
+
+	if err != nil {
+		log.Println("Error reading connection: ", err)
+		return
+	}
+
+	message := string(buffer[:])
+	// Split by new lines
+	splitLines := strings.Split(message, "\r\n")
+
+	// Split start line by empty spaces
+	startLine := strings.Split(splitLines[0], " ")
+
+	method := startLine[0]
+	uri := startLine[1]
+	protocolVersion := startLine[2]
+
+	log.Printf("Method %s, URI %s, Protocol Version %s", method, uri, protocolVersion)
+
+	for _, header := range splitLines[1:] {
+		fmt.Println(header)
+	}
+
+}
+
+func readMsg(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Println("Error reading message: ", err)
+		return
+	}
+
+	log.Println("Message: ", message)
+}
